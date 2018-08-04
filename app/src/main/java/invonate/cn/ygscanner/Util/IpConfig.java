5be06f5a71fe6eed326b7bc98928eb4ced5e206e
H-494 <614711977@qq.com>
2018-08-04 13:05:16 +0800

    提交

--- app/src/main/java/invonate/cn/ygscanner/Util/IpConfig.java
+++ app/src/main/java/invonate/cn/ygscanner/Util/IpConfig.java
@@ -5,4 +5,5 @@ public class IpConfig {
     //	public static final String  ip="10.89.14.17:9081";//
 //    public static final String ip = "10.10.4.210:9081";//测试
     	public static final String  ip="10.10.3.216:80";//正式
+    //fdsfa
 }
